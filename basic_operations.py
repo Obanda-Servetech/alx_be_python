9d52c034f74697c2fc7ba4e1f66cb2d8bc63d657
Steven Dzinkambani Banda <stevendbanda@yahoo.com>
2024-12-21 13:02:55 -0800

    python calc for biginner

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Define variables number1 and number2 with values 10 and 5
+number1 = 10
+number2 = 5
+
+# Perform addition, subtraction, and multiplication
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results in a human-readable format
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
